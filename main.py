1516e97eaecceaea54d714549370a3b6036c2696
TheAhmetu <reveallingg@gmail.com>
2025-07-14 01:46:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ sent_messages = []
 
 app = Flask('')
 
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run_flask)
+    t.daemon = True
+    t.start()
+
 @app.route('/')
 def home():
     return "Bot çalışıyor!"
